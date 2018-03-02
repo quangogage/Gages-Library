6f189075496d93795a4e87faa39bd4b6ba70ca32
Gage Henderson <gage@quangoinc.com>
2018-03-02 13:22:17 -0800

    Added  and  function to the math module.

--- Math.js
+++ Math.js
@@ -5,4 +5,14 @@ math.getRandom = function(min, max) {
   return Math.random() * (max - min + 1) + min;
 };
 
+// Convert to radians
+math.toRadians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+// Converts from radians to degrees.
+math.toDegrees = function(radians) {
+  return radians * 180 / Math.PI;
+};
+
 module.exports = math;
