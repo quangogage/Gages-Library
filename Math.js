324be09a12df10df45adc83b6df41f9c66222494
Gage Henderson <gage@quangoinc.com>
2018-07-11 14:41:45 -0700

    Added a math function

--- Math.js
+++ Math.js
@@ -7,12 +7,12 @@ math.getRandom = function(min, max) {
 
 // Convert to radians
 math.toRadians = function(degrees) {
-  return degrees * Math.PI / 180;
+  return (degrees * Math.PI) / 180;
 };
 
 // Converts from radians to degrees.
 math.toDegrees = function(radians) {
-  return radians * 180 / Math.PI;
+  return (radians * 180) / Math.PI;
 };
 
 // Get distance between two points
@@ -21,6 +21,42 @@ math.getDistance = function(x1, y1, x2, y2) {
   var b = y1 - y2;
 
   return Math.sqrt(a * a + b * b);
+};
+
+// Returns an object in a list selected by weighted
+// randomness.
+math.getWeightedFrom = function(list) {
+  var totalWeight = 0;
+  var selectedItem;
+
+  // Calculate the total combined weight of all
+  // items in the list.
+  for (var i = 0; i < list.length; i++) {
+    totalWeight += list[i].weight;
+  }
+
+  // Randomly pick a weight between 0 and the max
+  var randomSpawnWeight = math.getRandom(1, this.totalSpawnWeight);
+
+  // Make sure it is not larger than the maximum weight
+  if (randomSpawnWeight > totalWeight) {
+    randomSpawnWeight = totalWeight;
+  }
+
+  // Select from the randomly picked weight
+  // until below zero, then return the current
+  // item.
+  for (var i = 0; i < list.length; i++) {
+    var item = item[i];
+
+    randomWeight -= item.weight;
+    if (randomWeight <= 0) {
+      selectedItem = item;
+      break;
+    }
+  }
+
+  return item;
 };
 
 module.exports = math;
