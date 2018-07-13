6a63c0c57b63807bfc905b8c0097856d24bf60c4
Gage Henderson <gage@quangoinc.com>
2018-07-13 12:51:05 -0700

    Made randomization more precise

--- Math.js
+++ Math.js
@@ -36,7 +36,7 @@ math.getWeightedFrom = function(list) {
   }
 
   // Randomly pick a weight between 0 and the max
-  var randomWeight = math.getRandom(0, totalWeight);
+  var randomWeight = math.getRandom(1, totalWeight * 1000) / 1000;
 
   // Make sure it is not larger than the maximum weight
   if (randomWeight > totalWeight) {
