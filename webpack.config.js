b7653e9338ddf6b5dea349d3fb5d9246ecba6f4e
Gage Henderson <gage@quangoinc.com>
2018-08-16 13:41:36 -0700

    Now properly coonverting to es5?

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,19 @@ const path = require('path');
 
 module.exports = {
   entry: './gagelib.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      }
+    ]
+  },
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
